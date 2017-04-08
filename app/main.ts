2655b773b3985acada8338f159a9a9876062a09d
saurabhpati <saurabhpati.pati@gmail.com>
2017-04-08 15:53:02 +0530

    working hello world model

---
+++ app/main.ts
@@ -0,0 +1,4 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { EventsAppModule } from './app.module';
+
+platformBrowserDynamic().bootstrapModule(EventsAppModule);
