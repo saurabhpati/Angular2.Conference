36b36d132516ab17154808ead6d6617ad05d72ed
saurabhpati <saurabhpati.pati@gmail.com>
2017-04-08 19:33:44 +0530

    Creating a component to diplay events list

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { EventsAppComponent } from './events-app.component';
+import { EventsListComponent } from './events/events-list.component';
 
 @NgModule({
     imports: [BrowserModule],
-    declarations: [EventsAppComponent],
+    declarations: [EventsAppComponent, EventsListComponent],
     bootstrap: [EventsAppComponent]
 })
 
