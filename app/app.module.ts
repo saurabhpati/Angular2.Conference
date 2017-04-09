c4f04b7b2a369f80c00ef1bd23bebfdf2d367929
saurabhpati <saurabhpati.pati@gmail.com>
2017-04-09 22:57:38 +0530

    introducing a nav bar component into the view

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventsThumbNailComponent } from './events/events-thumbnail.component';
+import { NavBarComponent } from './nav/nav-bar.component'
 
 @NgModule({
     imports: [BrowserModule],
-    declarations: [EventsAppComponent, EventsListComponent, EventsThumbNailComponent],
+    declarations: [EventsAppComponent, 
+    EventsListComponent, 
+    EventsThumbNailComponent,
+    NavBarComponent],
     bootstrap: [EventsAppComponent]
 })
 
