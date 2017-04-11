c96210fd26d63226426fa2c60991050a04577c23
saurabhpati <saurabhpati.pati@gmail.com>
2017-04-12 00:01:06 +0530

    event details parameter id provided

--- app/events/event-details/event-details.component.ts
+++ app/events/event-details/event-details.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { EventsListService } from '../shared/events.service'
+import { EventsListService } from '../shared/events.service';
+import { ActivatedRoute } from '@angular/router'; 
 
 @Component({
     templateUrl: 'app/events/event-details/event-details.component.html',
@@ -8,7 +9,7 @@ import { EventsListService } from '../shared/events.service'
 
 export class EventDetailsComponent { 
     event;
-    constructor(private eventsService: EventsListService) {
-        this.event = this.eventsService.getEvent(1)
+    constructor(private eventsService: EventsListService, private route: ActivatedRoute) {
+        this.event = this.eventsService.getEvent(+this.route.snapshot.params['id']);
     }
 }
