98c378e67ee5f1ad0212382ea143f8b9d698feaa
Saurabh Pati <janardpx@legal.regn.net>
2017-05-22 12:01:15 +0530

    testing the http calls for post and delete

--- app/events/event-details/test/voter.service.spec.ts
+++ app/events/event-details/test/voter.service.spec.ts
@@ -12,16 +12,59 @@ describe('Voter Service', () => {
         voterService = new VoterService(mockHttp);
     });
 
-    it('Should delete a voter', () => {
-        let session = {
-            id: 6,
-            voters: ['joe', 'jonathan']
-        };
-        
-        mockHttp.delete.and.returnValue(Observable.of(false));
-        voterService.deleteUserVote(3, <ISession>session, 'joe');
+    describe('deleteUserVote', () => {
+        it('Should delete a voter', () => {
+            let session = {
+                id: 6,
+                voters: ['joe', 'jonathan']
+            };
 
-        expect(session.voters.length).toBe(1);
-        expect(session.voters[0]).toBe('jonathan');
+            mockHttp.delete.and.returnValue(Observable.of(false));
+            voterService.deleteUserVote(3, <ISession>session, 'joe');
+
+            expect(session.voters.length).toBe(1);
+            expect(session.voters[0]).toBe('jonathan');
+        });
+
+        it('Should call http.delete correctly', () => {
+            let session = {
+                id: 6,
+                voters: ['joe', 'jonathan']
+            };
+
+            mockHttp.delete.and.returnValue(Observable.of(false));
+            voterService.deleteUserVote(3, <ISession>session, 'joe');
+
+            expect(mockHttp.delete).toHaveBeenCalledWith('/api/events/3/sessions/6/voters/joe');
+        });
     });
+
+    describe('addUserVote', () => {
+        it('should add a voter', () => {
+            let session = {
+                id: 6,
+                voters: ['jonathan']
+            };
+
+            mockHttp.post.and.returnValue(Observable.of(false));
+            voterService.addUserVote(3, <ISession>session, 'joe');
+
+            expect(session.voters.length).toBe(2);
+            expect(session.voters[1]).toBe('joe');
+        });
+
+        it('should add http.post correctly', () => {
+            let session = {
+                id: 6,
+                voters: ['jonathan']
+            };
+
+            mockHttp.post.and.returnValue(Observable.of(false));
+            voterService.addUserVote(3, <ISession>session, 'joe');
+
+            expect(mockHttp.post).toHaveBeenCalledWith('/api/events/3/sessions/6/voters/joe', {}, jasmine.any(Object));
+        });
+    });
+
+
 })
